93b226e99fb9d71b30bf702ba9a1e3a0482412e9
Torkelz <and_re1@hotmail.com>
2014-04-28 11:42:00 +0200

    Did some testing of the new objects and filters

--- ToolKit/Source/MainWindow.h
+++ ToolKit/Source/MainWindow.h
@@ -15,6 +15,13 @@ public:
 	explicit MainWindow(QWidget *parent = 0);
 	~MainWindow();
 
+private slots:
+    void on_m_ObjectTreeAddButton_clicked();
+
+    void on_m_ObjectTreeRemoveButton_clicked();
+
+    void on_actionObject_Tree_triggered();
+
 private:
 	Ui::MainWindow *ui;
 };
