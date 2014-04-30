98211fb66b4939a8f626594204e5b1ae29e3f027
Torkelz <and_re1@hotmail.com>
2014-04-30 15:16:55 +0200

    Added object icon to the app.

--- ToolKit/Source/TreeItem.cpp
+++ ToolKit/Source/TreeItem.cpp
@@ -8,4 +8,5 @@ TreeItem::TreeItem(std::string p_ItemName)
 {
     setText(0, QString::fromStdString(p_ItemName));
     setFlags(Qt::ItemIsDragEnabled | Qt::ItemIsEditable | Qt::ItemIsSelectable | Qt::ItemIsEnabled);
+    setIcon(0, QIcon(":/Icons/Assets/object.png"));
 }
