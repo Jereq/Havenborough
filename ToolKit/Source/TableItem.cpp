4b0e55907710aa028bb196cf28e41433a5b82eb4
Sebastian Larsson <sjereq@gmail.com>
2014-05-14 12:48:52 +0200

    Revert linebreaking object names for now

--- ToolKit/Source/TableItem.cpp
+++ ToolKit/Source/TableItem.cpp
@@ -6,9 +6,6 @@ TableItem::TableItem()
 
 TableItem::TableItem(std::string p_ItemName)
 {
-	if(p_ItemName.length() > 8)
-		p_ItemName.insert(8, "-\n");
-
     setText(QString::fromStdString(p_ItemName));
     setTextAlignment(Qt::AlignBottom | Qt::AlignCenter);
     setIcon(QIcon(":/Icons/Assets/object.png"));
